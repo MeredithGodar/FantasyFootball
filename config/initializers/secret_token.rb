6dc5721c8a3daa8953e3111b82bdaa46db3744a0
godar <meredith-godar@uiowa.edu>
2013-10-21 11:55:19 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantasyfootball::Application.config.secret_token = '52b2874b67f2f108e06dda03f29a66a84ae920046fc1cd62c0c87987efc28a3d33d268c1719a2f232c6b058aabc8d80e08b22c7418638baa4b74944dbff1b60c'
