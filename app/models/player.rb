38e090029dcf80ad3a38c068e234fd9368d6b98d
Fryman1991 <Christopher-J-Fry@uiowa.edu>
2013-11-07 19:48:57 -0600

    Starting Player functionality

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,5 @@
 class Player < ActiveRecord::Base
+  def self.all_positions
+    %w(QB TE RB WR DE K)
+  end
 end
--- db/migrate/20131108001055_create_players.rb
+++ db/migrate/20131108001055_create_players.rb
@@ -1,7 +1,14 @@
 class CreatePlayers < ActiveRecord::Migration
   def up
+    create_table :players do |t|
+      t.string :name
+      t.string :position
+      t.string :team
+      t.timestamps
+    end
   end
 
   def down
+    drop_table :players
   end
 end
