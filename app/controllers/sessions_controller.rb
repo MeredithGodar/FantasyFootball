2b7f9c751e1b8a3f24bb2d3f3d00f6d54c5d789b
Fryman1991 <Christopher-J-Fry@uiowa.edu>
2013-11-10 22:27:19 -0600

    heroku attempt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
 	end
 	
 	def create
-	  puts("USER EMAIL?")
-	  puts(params[:session][:email])
-	  puts("USER PASS?")
-	  puts(params[:session][:password])
+	  #puts("USER EMAIL?")
+	  #puts(params[:session][:email])
+	  #puts("USER PASS?")
+	  #puts(params[:session][:password])
 		#user = User.find_by_email(params[:session][:email])
-		puts("THIS IS THE USER")
-		puts(user)
+		#puts("THIS IS THE USER")
+		#puts(user)
 		#if user == nil
 		  auth = request.env["omniauth.auth"]
 		  puts ("THIS IS AUTH")
@@ -36,8 +36,8 @@ class SessionsController < ApplicationController
 		session[:session_token] = nil
 		#cookies.delete(:session_token)
 		#@current_user = nil
-		#flash[:notice] = 'You have logged out'
-		redirect_to players_path
+		flash[:notice] = 'You have logged out'
+		redirect_to users_path
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ActiveRecord::Base
 #<<<<<<< HEAD
   # attr_accessible :title, :body
-  has_many :players
+  #has_many :players
 #=======
 	attr_accessible :user_id, :email, :password, :password_confirmation
-	has_secure_password
+	#has_secure_password
 #>>>>>>> faf9790dd038c5772dd2159da3afc1ebc94fabd7
   
-  before_save{|user| user.email=user.email.downcase}
+  #before_save{|user| user.email=user.email.downcase}
   before_save :create_session_token
-  validates :user_id, presence: true, length:{maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: 8}
-  validates :password_confirmation, presence: true
+  #validates :user_id, presence: true, length:{maximum: 50}
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  #validates :password, presence: true, length: {minimum: 8}
+  #validates :password_confirmation, presence: true
   
   private
   def create_session_token
