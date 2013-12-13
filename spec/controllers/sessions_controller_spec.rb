b70dd5b9a342fe392cdc24211609fa48c07f621c
jacorso <jared-corso@uiowa.edu>
2013-12-12 23:08:49 -0800

    ak;jsdf;al

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 
 describe SessionsController do
 
+  describe 'new' do
+    SessionsController.new()
+  end
+
+  describe 'create' do
+    SessionsController.create()
+  end
+
+  describe 'destroy' do
+    SessionsController.destroy()
+  end
+
 end
